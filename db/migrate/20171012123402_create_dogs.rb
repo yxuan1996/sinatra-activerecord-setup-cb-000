3be0bfa7a18e6aaa4545ceb18ed2d68e50a854ae
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-12 12:44:10 +0000

    automatically backed up by learn

--- db/migrate/20171012123402_create_dogs.rb
+++ db/migrate/20171012123402_create_dogs.rb
@@ -1,4 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
-  def change
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+ 
+  def down
+    drop_table :dogs
   end
 end
